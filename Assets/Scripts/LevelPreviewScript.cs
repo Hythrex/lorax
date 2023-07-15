176d99ae8232f04a9e5c7392d374f33c595f219f
Hythraux <86388177+Hythrex@users.noreply.github.com>
2023-07-15 16:38:18 -0400

    Add files via upload

---
+++ Assets/Scripts/LevelPreviewScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelPreviewScript : MonoBehaviour
+{
+    public GameObject top;
+    public GameObject bottom;
+
+    public AudioSource aSource;
+    public AudioClip bellSound;
+
+    public TMP_Text text1; // the gameobject and this is different
+    public TMP_Text text2;
+
+    private int weekLevel;
+    
+
+    private void Start()
+    {
+        weekLevel = PlayerPrefs.GetInt("week");
+        top.SetActive(false);
+        bottom.SetActive(false);
+
+        this.changetext();
+
+        StartCoroutine(showText());
+    }
+
+    private IEnumerator showText()
+    {
+        Debug.Log("The player is on week " + weekLevel + "!");
+        yield return new WaitForSeconds(1.5f);
+        top.SetActive(true);
+        aSource.PlayOneShot(bellSound, 1);
+        yield return new WaitForSeconds(4f);
+        bottom.SetActive(true);
+        aSource.PlayOneShot(bellSound, 1);
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Level1");
+    }
+
+    private void changetext()
+    {
+        text1.text = "Week " + weekLevel;
+        if (weekLevel == 2)
+        {
+            text2.text = "keep chopping down the trees";
+        }
+        else if (weekLevel  == 3)
+        {
+            text2.text = "stop chopping down the trees";
+        }
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    // excuse my terrible code
+    public GameObject selectHolder1;
+    public GameObject selectHolder2;
+    public GameObject label;
+
+    public GameObject anim1;
+    public GameObject anim2;
+
+    public TMP_Text label_text;
+
+    private void Start()
+    {
+        selectHolder1.SetActive(false);
+        selectHolder2.SetActive(false);
+        label.SetActive(false);
+
+        StartCoroutine(Anims());
+    }
+
+    private void Update()
+    {
+        // this is just temporary
+        // im gonna comment this soon
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            PlayerPrefs.SetInt("week", 3);
+            PlayerPrefs.Save();
+            Debug.Log("hi");
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            PlayerPrefs.SetInt("week", 1);
+            PlayerPrefs.Save();
+            Debug.Log("bru");
+        }
+    }
+
+    private IEnumerator Anims()
+    {
+        /* i need a more effecient way to do this 
+        should probably make it an animation and make it play forever */
+        anim1.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+
+        anim2.SetActive(true);
+        anim1.SetActive(false);
+        yield return new WaitForSeconds(0.05f);
+
+        anim2.SetActive(false);
+        anim1.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+
+        anim2.SetActive(true);
+        anim1.SetActive(false);
+        yield return new WaitForSeconds(0.01f);
+
+        anim2.SetActive(false);
+        anim1.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+
+        anim2.SetActive(true);
+        anim1.SetActive(false);
+        yield return new WaitForSeconds(0.05f);
+
+        anim2.SetActive(false);
+        anim1.SetActive(true);
+
+        StartCoroutine(Anims()); /* basically loop it forever */
+    }
+
+    public void continueButton()
+    {
+        SceneManager.LoadScene("Loadthing");
+    }
+
+    public void startNew()
+    {
+        PlayerPrefs.SetInt("week", 1); // this is necessary
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("Loadthing");
+    }
+
+    public void showMouseSelect1()
+    {
+        selectHolder1.SetActive(true);
+    }
+
+    public void removeMouseSelect1()
+    {
+        selectHolder1.SetActive(false);
+    }
+
+    public void showMouseSelect2()
+    {
+        selectHolder2.SetActive(true);
+        label.SetActive(true);
+        label_text.text = "Week " + PlayerPrefs.GetInt("week");
+    }
+
+    public void removeMouseSelect2()
+    {
+        selectHolder2.SetActive(false);
+        label.SetActive(false);
+    }
+
+}
